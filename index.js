f11ff9368d601ef012ebfa2ece7e3b28455134b3
AyoNeo123 <valeronisbestww@gmail.com>
2026-08-06 13:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -381,7 +381,7 @@ client.on('messageCreate', async message => {
     }
 
     // --- STREAM SCHEDULE LOGIC ---
-    if (content.includes('stream') && content.includes('bunji')) {
+    if ((content.includes('stream') && content.includes('bunji')) || content.includes('when stream')) {
         try {
             const streamCheck = await groq.chat.completions.create({
                 messages: [
@@ -1665,7 +1665,7 @@ client.on('interactionCreate', async interaction => {
             
             const ticketType = parts[1];
             let detailsEmbed = new EmbedBuilder().setColor('#2F3136');
-            let contentMsg = `<@${interaction.user.id}> Welcome, support will be with you shortly.\n**Staff has been notified, Please Wait**`;
+            let contentMsg = `<@${interaction.user.id}> <@&1373246947732885684> Welcome, support will be with you shortly.\n**Staff has been notified, Please Wait**`;
 
             if (ticketType === 'general') {
                 const reason = interaction.fields.getTextInputValue('reason').substring(0, 1000);
